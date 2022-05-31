1d10807e7340a7b6860b8738e59841507799e63f
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-31 01:21:10 -0300

    Atualiacao de sessions controller, helper e adicionando autenticacao com mensagens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
-end
+    include SessionsHelper
+  
+    def authorize
+      unless logged_in?
+        redirect_to root_path
+      end
+    end
+  
+    def correct_usuario?
+      @usuario = Usuario.find(params[:id])
+      unless current_usuario == @usuario
+        redirect_to usuarios_path
+      end
+    end
+  end
+  
