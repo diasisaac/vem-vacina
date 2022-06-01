f2e2909934b7cf1f68232d1dae1006a32bc2aa3c
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-31 23:43:59 -0300

    Usuario feature e usuaio_steps

---
+++ features/step_definitions/usuario_steps.rb
@@ -0,0 +1,75 @@
+
+Given('Eu estou na pagina de cadastro de usuario') do
+  visit "/usuarios/new"
+  expect(page).to have_current_path('/usuarios/new')
+end
+
+Given('Eu preencho o cadastro como role {string}, nome completo {string}, data de nascimento: {string}, cpf: {string}, telefone: {string}, nome completo da mae {string}, numero do sus: {string}, email: {string}, password {string}, cidade: {string}, logradouro: {string},complemento: {string}, bairro: {string},cep: {string}') do |role, nome_completo, data_de_nascimento, cpf, telefone, nome_mae, num_sus, email, password, cidade, logradouro, complemento, bairro, cep|
+  fill_in 'usuario[nome_completo]', :with => nome_completo
+  fill_in 'usuario[data_nascimento]', :with => data_de_nascimento
+  fill_in 'usuario[cpf]', :with => cpf
+  fill_in 'usuario[telefone]', :with => telefone
+  fill_in 'usuario[nome_mae]', :with => nome_mae
+  fill_in 'usuario[num_sus]', :with => num_sus
+  fill_in 'usuario[email]', :with => email
+  fill_in 'usuario[num_sus]', :with => num_sus
+  fill_in 'usuario[password]', :with => password
+  fill_in 'usuario_endereco_attributes_cep',	with: cep
+  fill_in 'usuario_endereco_attributes_cidade',	with: cidade
+  fill_in 'usuario_endereco_attributes_bairro',	with: bairro
+  fill_in 'usuario_endereco_attributes_logradouro',	with: logradouro
+  fill_in 'usuario_endereco_attributes_complemento',	with: complemento
+
+  select(role, from: 'usuario[role]')
+
+end
+
+When('Eu clico em cadastrar') do
+  click_button 'Create Usuario'
+end
+
+Then('Eu vejo a mensagem de que o usuario foi cadastrado') do
+  expect(page).to have_content("Usuario cadastrado")
+end
+
+
+Given('O usuario com role {string}, nome completo {string}, data de nascimento: {string}, cpf: {string}, telefone: {string}, nome completo da mae {string}, numero do sus: {string}, email: {string}, password {string}, cidade: {string}, logradouro: {string},complemento: {string}, bairro: {string},cep: {string} existe') do |role, nome_completo, data_de_nascimento, cpf, telefone, nome_mae, num_sus, email, password, cidade, logradouro, complemento, bairro, cep|
+  visit "/usuarios/new"
+  expect(page).to have_current_path('/usuarios/new')
+  fill_in 'usuario[nome_completo]', :with => nome_completo
+  fill_in 'usuario[data_nascimento]', :with => data_de_nascimento
+  fill_in 'usuario[cpf]', :with => cpf
+  fill_in 'usuario[telefone]', :with => telefone
+  fill_in 'usuario[nome_mae]', :with => nome_mae
+  fill_in 'usuario[num_sus]', :with => num_sus
+  fill_in 'usuario[email]', :with => email
+  fill_in 'usuario[num_sus]', :with => num_sus
+  fill_in 'usuario[password]', :with => password
+  fill_in 'usuario_endereco_attributes_cep',	with: cep
+  fill_in 'usuario_endereco_attributes_cidade',	with: cidade
+  fill_in 'usuario_endereco_attributes_bairro',	with: bairro
+  fill_in 'usuario_endereco_attributes_logradouro',	with: logradouro
+  fill_in 'usuario_endereco_attributes_complemento',	with: complemento
+
+  select(role, from: 'usuario[role]')
+  click_button 'Create Usuario'
+
+end
+
+Given('Eu estou na pagina de login') do
+  expect(page).to have_current_path('/')
+end
+
+Given('Eu preencho login como cpf {string} e senha com {string}') do |cpf, senha|
+  visit "/sign_in"
+  fill_in 'session[cpf]', :with => cpf
+  fill_in 'session[password]', :with => senha
+end
+
+When('Eu Clico em entrar') do
+  click_button 'Entrar'
+end
+
+Then('Eu vejo que estou na pagina do meu usuario') do
+  expect(page).to have_content("Show de Usuario")
+end
---
+++ features/usuario.feature
@@ -0,0 +1,18 @@
+Feature: Usuario
+
+  As a usuario do sistema vem-vacina
+  I want to me cadastrar, logar e editar meu cadastro
+  so that eu nao tenha que fazer isso manualmente
+
+  Scenario: cadastrar novo usuario no sistema
+    Given Eu estou na pagina de cadastro de usuario
+    And Eu preencho o cadastro como role 'Usuario', nome completo 'Tarcisio de Creta', data de nascimento: '1980-10-10', cpf: '11111111112', telefone: '81955555555', nome completo da mae 'Virginia de Souza', numero do sus: '12345678', email: 'tarcisio@gmail.com', password '123456', cidade: 'Recife', logradouro: 'rua rosa',complemento: 'casa forte', bairro: 'Boa Viagem',cep: '55555555'
+    When Eu clico em cadastrar
+    Then Eu vejo a mensagem de que o usuario foi cadastrado
+
+  Scenario: logar no sistema
+    Given O usuario com role 'Usuario', nome completo 'Tarcisio de Creta', data de nascimento: '1980-10-10', cpf: '12345678915', telefone: '81955555555', nome completo da mae 'Virginia de Souza', numero do sus: '12345678', email: 'tarcisio@gmail.com', password '123456', cidade: 'Recife', logradouro: 'rua rosa',complemento: 'casa forte', bairro: 'Boa Viagem',cep: '55555555' existe
+    Given Eu estou na pagina de login
+    And Eu preencho login como cpf "12345678915" e senha com "123456"
+    When Eu Clico em entrar
+    Then Eu vejo que estou na pagina do meu usuario
