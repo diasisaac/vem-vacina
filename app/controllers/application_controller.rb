ec2a32049499238590aa367d171192123d05c57c
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-31 22:55:37 -0300

    Atualizacao de application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,22 @@ class ApplicationController < ActionController::Base
     def correct_usuario?
       if logged_in?
         @usuario = Usuario.find(params[:id])
-        unless current_usuario == @usuario
+        unless current_usuario == @usuario or current_usuario.Admin?
           redirect_to @usuario
         end
       else
+        redirect_to root_path
+      end
+    end
+
+    def authorize_admin
+      if logged_in?
+        unless current_usuario.Admin?
+          flash[:danger] = "Voce nao tem privilegios no sistema"
+          redirect_to current_usuario
+        end
+      else
+        flash[:danger] = "Usuario deslogado"
         redirect_to root_path
       end
     end
